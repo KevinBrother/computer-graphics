e833d48e23d45658a1b0a2c76dab877893fb37b8
caojunjie <1301239018@qq.com>
2024-05-27 15:43:31 +0800

    fix(fs-by-vs): 片段着色器使用顶点着色器中的顶点数据作为颜色

---
+++ src/03-shaders/06-fs-by-vs/resources/shader.vs.glsl
@@ -0,0 +1,9 @@
+#version 330 core
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aColor;
+out vec4 vertexColor; // 为片段着色器指定一个颜色输出
+void main()
+{
+  gl_Position = vec4(aPos, 1.0); // 把 一个 vec3 作为 vec4 的构造器的参数
+  vertexColor = vec4(aPos, 1.0);
+}
