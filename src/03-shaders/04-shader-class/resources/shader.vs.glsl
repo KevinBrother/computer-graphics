33d4ab36a46fb7256d14dfd65b6efa1df1e2b9f1
caojunjie <1301239018@qq.com>
2024-05-27 14:14:15 +0800

    fix(shader): 修改顶点着色器代码，使用顶点属性数据指定的值

--- src/03-shaders/04-shader-class/resources/shader.vs.glsl
+++ src/03-shaders/04-shader-class/resources/shader.vs.glsl
@@ -1,8 +1,9 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aColor;
 out vec4 vertexColor; // 为片段着色器指定一个颜色输出
 void main()
 {
-  gl_Position = vec4(aPos.x, -aPos.y, aPos.z, 1.0); // 把 一个 vec3 作为 vec4 的构造器的参数
-  vertexColor = vec4(0.5, 0.0, 0.0, 1.0); // 把输出变量设置为暗红色
+  gl_Position = vec4(aPos, 1.0); // 把 一个 vec3 作为 vec4 的构造器的参数
+  vertexColor = vec4(aColor, 1.0);
 }
