0431e39dda812631898ed06518549d1a10515173
caojunjie <1301239018@qq.com>
2024-05-28 19:55:41 +0800

    features(texture): 笑脸旋转到另一个方向

--- src/04-textures/02-more-textures/resources/shader.fs.glsl
+++ src/04-textures/02-more-textures/resources/shader.fs.glsl
@@ -10,4 +10,7 @@ void main()
 //  FragColor = texture(texture2, texCoord);
   // 0.2会返回80%的第一个输入颜色和20%的第二个输入颜色，即返回两个纹理的混合色。 0 返回第一个输入颜色，1 返回第二个输入颜色。
    FragColor = mix(texture(texture1, texCoord), texture(texture2, texCoord), 0.2);
+
+// 旋转笑脸到另一个方向
+   FragColor = mix(texture(texture1, texCoord), texture(texture2, vec2(1.0 - texCoord.x, texCoord.y)), 0.2);
 }
