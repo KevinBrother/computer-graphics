c060a9b497f96b3d3ac93c5e0fba56f594a60cb4
caojunjie <1301239018@qq.com>
2024-06-03 14:27:41 +0800

    feat(coordinate): 简单的 3D 效果

---
+++ src/06-coordinate/01-3d/resources/shader.vs.glsl
@@ -0,0 +1,17 @@
+#version 330 core
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aColor;
+layout (location = 2) in vec2 aTexCoord;
+out vec3 vertexColor; // 为片段着色器指定一个颜色输出
+out vec2 texCoord; // 为片段着色器指定一个纹理坐标输出
+
+uniform mat4 model; // 模型矩阵
+uniform mat4 view; // 视图矩阵
+uniform mat4 projection; // 投影矩阵
+
+void main()
+{
+  gl_Position = projection * view * model * vec4(aPos, 1.0); // 对顶点位置进行变换
+  vertexColor = aColor;
+  texCoord = aTexCoord;
+}
